3fbbb5e777d61e7691c1267f97212c9bc5897313
Arjun Haridas <arjunharidas@carestack.com>
2023-06-10 19:54:08 +0530

    Add project files.

---
+++ MainApplication/Models.cs
@@ -0,0 +1,16 @@
+﻿namespace MainApplication
+{
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ MainApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapperClone;
+
+namespace MainApplication
+{
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Employee employee = new Employee() { FirstName = "Arjun", LastName = "Haridas", EmployeeId = 10 };
+            Person person = SlowMapper.Map<Employee, Person>(employee);
+
+            Console.WriteLine(person.FirstName);
+            Console.WriteLine(person.LastName);
+        }
+    }
+}
+
